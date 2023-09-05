44adbf2f16a59140736c892363dc5cbd6d3530f5
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-09-05 18:06:35 +0200

    edited code

--- app.js
+++ app.js
@@ -12,3 +12,22 @@ function searchGitHubUser() {
 
 const searchButton = document.getElementById('searchButton');
 searchButton.addEventListener('click', searchGitHubUser);
+// mock data to filter with the searchInput element value
+const mockArray = ['mile', 'maki', 'sumi', 'maja', 'goran', 'beka']
+
+// called on searchButton click
+function searchGitHubUser() {
+  // gets the current value of the searchInput element
+  const searchInput = document.getElementById('searchInput').value;
+  // result element to show the final filtration result of mock data
+  const resultElement = document.querySelector('.card-body');
+
+  let filteredArray = []
+  // TODO: filter the mock array to only include searchInput values
+  // google array.include, array.filter
+
+  resultElement.textContent = filteredArray;
+}
+
+const searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', searchGitHubUser);
